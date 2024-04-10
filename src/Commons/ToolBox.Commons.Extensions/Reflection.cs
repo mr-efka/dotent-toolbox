60edfad6e7443ea79132bae6bbc2e19fb2e14fd1
Mr Efka <kamgangf11@outlook.fr>
2024-04-10 12:20:45 +0200

    feat(Commons.Extensions): Add custom reflection extension methods

---
+++ src/Commons/ToolBox.Commons.Extensions/Reflection.cs
@@ -0,0 +1,20 @@
+using System.Reflection;
+
+namespace MrEfka.ToolBox.Commons.Extensions;
+
+public static class Reflection
+{
+    /// <summary>
+    /// Checks whether the provided <paramref name="memberInfo"/> has attributes of type <typeparamref name="T"/> in his custom attributes collection.
+    /// </summary>
+    /// <typeparam name="T">Type of the attribute to check for.</typeparam>
+    /// <param name="memberInfo">Member to evaluate.</param>
+    /// <param name="inherit"><inheritdoc cref="MemberInfo.GetCustomAttributes(bool)"/></param>
+    /// <returns><see langword="true"/> if the attribute is defined on the <see cref="MemberInfo"/>, otherwise <see langword="false"/></returns>
+    public static bool HasAttributesOfType<T>(this MemberInfo memberInfo, bool inherit = true) where T : Attribute
+    {
+        ArgumentNullException.ThrowIfNull(memberInfo);
+        var attrs = memberInfo.GetCustomAttributes<T>(inherit);
+        return attrs.Any();
+    }
+}
